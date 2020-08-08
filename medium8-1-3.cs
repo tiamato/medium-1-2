c7c8c0e46cf30d5f3b361e985d0a68e4216da4ab
adavletyarov <adavletyarov@fors.ru>
2020-08-08 03:36:47 +0300

    Решение

--- medium8-1-3.cs
+++ medium8-1-3.cs
@@ -1,25 +1,78 @@
-class Bag
-{
-    public List<Item> Items;
-    public int MaxWeidth; 
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
-    public void AddItem(string name, int count)
+namespace Bag
+{
+    class Bag
     {
-        int currentWeidth = Items.Sum(item => item.Count);
-        Item targetItem = Items.FirstOrDefault(item => item.Name == name);
+        private readonly int _maxWeigth;
+        private readonly List<Item> _items = new List<Item>();
 
-        if (targetItem == null)
-            throw new InvalidOperationException();
+        public ReadOnlyCollection<ReadOnlyItem> Items => _items.ConvertAll(Item.ToReadOnlyItem).AsReadOnly();
 
-        if (currentWeidth + count > MaxWeidth)
-            throw new InvalidOperationException();
+        public Bag(int maxWeigth, Item[] items)
+        {
+            _maxWeigth = maxWeigth;
 
-        targetItem.Count += count;
+            foreach (Item item in items)
+            {
+                _items.Add(new Item(item.Name, item.Count));
+            }
+        }
+
+        public void AddItem(string name, int count)
+        {
+
+            Item targetItem = _items.FirstOrDefault(item => item.Name == name);
+
+            if (targetItem == null)
+                throw new InvalidOperationException();
+
+            if (GetCurrentWeigth() + count > _maxWeigth)
+                throw new InvalidOperationException();
+
+            targetItem.AddCount(count);
+        }
+
+        private int GetCurrentWeigth()
+        {
+            return _items.Sum(item => item.Count);
+        }
     }
-}
 
-class Item
-{
-    public int Count;
-    public string Name;
+    public class Item
+    {
+        public int Count { private set; get; }
+        public string Name { get; }
+
+        public Item(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public void AddCount(int count)
+        {
+            Count += count;
+        }
+
+        public static ReadOnlyItem ToReadOnlyItem(Item item)
+        {
+            return new ReadOnlyItem(item);
+        }
+    }
+
+    public class ReadOnlyItem
+    {
+        private readonly Item _item;
+        public int Count => _item.Count;
+        public string Name => _item.Name;
+
+        public ReadOnlyItem(Item item)
+        {
+            _item = item;
+        }
+    }
 }
